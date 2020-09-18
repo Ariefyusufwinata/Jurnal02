296ca3182f7fc02bc5c15d9a47006e9db4c1436e
Ariefyusufwinata <winata.arief26@gmail.com>
2020-09-18 14:09:01 +0700

    Jurnal02

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld{
+	
+	public static void sayHello(){
+		System.out.println("Nama  : Arief Yusuf Winata");
+		System.out.println("NIM   : 6706201046");
+		System.out.println("Kelas : 44-01");
+	}
+
+	public static void main (String[] args){
+		sayHello();
+	}
+}
